03d1f3efe402416e73fc4ffdfce445c7c7ebb8f8
Igor Silva <igorqa2022@outlook.com>
2024-04-06 01:16:46 -0300

    feat: Subindo a  pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '6wi5fs',
+  projectId: '43jasi',
   e2e: {
     baseUrl: 'https://api.restful-api.dev',
     setupNodeEvents(on, config) {
